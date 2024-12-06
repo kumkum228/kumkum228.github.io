264d3164a22d7bbad60739263a4692504ef9a5ad
Kumkum Kumari <kumkum981@gmail.com>
2024-12-06 16:54:32 +0000

    Add files via upload
    Added Portfolio project

---
+++ portfolio/Projects/4. Birth-Day/script.js
@@ -0,0 +1,110 @@
+function show() {
+    /* Access image by id and change the display property to block*/
+    document.getElementById('bulb')
+        .style.display = "block";
+    document.getElementById('btnID')
+        .style.display = "none";
+}
+
+function btnShow(){
+    document.getElementById('demo').style.display='block';
+}
+
+function changeImage() {
+    var image1 = document.getElementById('light1');
+    if (image1.src.match("bulbon")) {
+        image1.src = "img/bulboff.gif";
+    } else {
+    image1.src = "img/bulbon.gif";
+    }
+}
+function changeImage2() {
+    var image2 = document.getElementById('light2');
+    if (image2.src.match("bulbon")) {
+        image2.src = "img/bulboff.gif";
+    } else {
+    image2.src = "img/bulbon.gif";
+
+    }
+} 
+function changeImage3() {
+    var image3 = document.getElementById('light3');
+    if (image3.src.match("bulbon")) {
+        image3.src = "img/bulboff.gif";
+    } else {
+    image3.src = "img/bulbon.gif";
+
+    }
+} 
+function changeImage4() {
+    var image4 = document.getElementById('light4');
+    if (image4.src.match("bulbon")) {
+        image4.src = "img/bulboff.gif";
+    } else {
+    image4.src = "img/bulbon.gif";
+    }
+} 
+function changeImage5() {
+    var image5 = document.getElementById('light5');
+    if (image5.src.match("bulbon")) {
+        image5.src = "img/bulboff.gif";
+    } else {
+    image5.src = "img/bulbon.gif";
+    }
+} 
+function changeImage6() {
+    var image6 = document.getElementById('light6');
+    if (image6.src.match("bulbon")) {
+        image6.src = "img/bulboff.gif";
+    } else {
+    image6.src = "img/bulbon.gif";
+    }
+} 
+function changeImage7() {
+    var image7 = document.getElementById('light7');
+    if (image7.src.match("bulbon")) {
+        image7.src = "img/bulboff.gif";
+    } else {
+    image7.src = "img/bulbon.gif";
+    }
+} 
+function changeImage8() {
+    var image8 = document.getElementById('light8');
+    if (image8.src.match("bulbon")) {
+        image8.src = "img/bulboff.gif";
+    } else {
+    image8.src = "img/bulbon.gif";
+    }
+} 
+function changeImage9() {
+    var image9 = document.getElementById('light9');
+    if (image9.src.match("bulbon")) {
+        image9.src = "img/bulboff.gif";
+    } else {
+    image9.src = "img/bulbon.gif";
+    }
+} 
+function changeImage10() {
+    var image10 = document.getElementById('light10');
+    if (image10.src.match("bulbon")) {
+        image10.src = "img/bulboff.gif";
+    } else {
+    image10.src = "img/bulbon.gif";
+    }
+} 
+function changeImage11() {
+    var image11 = document.getElementById('light11');
+    if (image11.src.match("bulbon")) {
+        image11.src = "img/bulboff.gif";
+    } else {
+    image11.src = "img/bulbon.gif";
+    }
+} 
+function changeImage12() {
+    var image12 = document.getElementById('light12');
+    if (image12.src.match("bulbon")) {
+        image12.src = "img/bulboff.gif";
+    } else {
+    image12.src = "img/bulbon.gif";
+    }
+} 
